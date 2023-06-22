6e2d44800e94f4b25eed6bb39c228a785ed9d16c
Imad Jhabli <imadjhabli@gmail.com>
2023-06-22 03:31:14 -0700

    3fixed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
