1c4992de0f1ecae98a67314698c9c9f1fd6f209f
Imad Jhabli <imadjhabli@gmail.com>
2023-06-22 04:54:33 -0700

    5fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,4 +2,22 @@
 /**
  * more_numbers - prints 10 times from 0 to 14
  *
-	 *
+ * Return: none
+ */
+void more_numbers(void)
+{
+	char i;
+	int j;
+
+	for ( j = 1 ; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
