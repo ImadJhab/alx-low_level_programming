16d1656621d223a1170330b38199dbdf6f3df155
Imad Jhabli <imadjhabli@gmail.com>
2023-06-26 15:57:55 -0700

    Redemption on atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,18 +12,12 @@ int _atoi(char *s)
 	while (*s)
 	{
 		if (*s == '-')
-		{
 			sign = -sign;
-		}
 		else if (*s >= '0' && *s <= '9')
-		{
 			result = result * 10 + (*s - '0');
-		}
 		else if (result > 0)
-		{
 			break;
 			s++;
-		}
 	}
 	return (sign * result);
 }
