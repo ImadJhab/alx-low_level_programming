7ce4b1d383797f6e594374f4c99511ceffec5889
Imad Jhabli <imadjhabli@gmail.com>
2023-06-29 13:35:19 -0700

    1fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,18 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	int dest_len = 0;
+	int i = 0;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	while (dest[dest_len] != '\0')
 	{
-		for (i = 0; i < n && src[i] != '\0'; i++)
-		{
-			dest[dest_len + i] = src[i];
-		}
-		dest[dest_len + i] = '\0';
+		dest_len++;
 	}
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
