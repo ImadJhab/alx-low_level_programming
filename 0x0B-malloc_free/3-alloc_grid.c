2a1db97a013f04a075f5cd649480a1f81a0792a5
Imad Jhabli <imadjhabli@gmail.com>
2023-07-10 08:00:24 -0700

    3done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 		return (0);
 	}
 
-	grid = (int **) malloc(height * sizeof(int));
+	grid = (int **) malloc(height * sizeof(int *));
 
 	if (grid == 0)
 	{
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *) malloc(width * sizeof(int));
 		if (grid[i] == 0)
 		{
-			for (j = 0; j < width; j++)
+			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
 			}
