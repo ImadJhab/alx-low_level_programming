edc7edca01e87aec821d446c221e7303bf59ff2c
Imad Jhabli <imadjhabli@gmail.com>
2023-07-17 14:27:09 -0700

    4woof

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dog
+ * @d: pointer of struct
+ * Return: none
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
