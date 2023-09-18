209a8c8fe8eba0ccc7f2ee95aa4ce3ab5698b462
Imad Jhabli <imadjhabli@gmail.com>
2023-09-18 15:20:07 +0100

    done

---
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any
+ * of a set of bytes
+ * @s: first string
+ * @accept: second string
+ * Return:pointer to s that matches in accept,
+ * 0 if no byte found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (0);
+}
